a7fafd15085e0c983b03105b0b66cf2eb0818b2b
shreya02c <shreyachaudhari996@gmail.com>
2025-03-18 13:29:56 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div>
+            <p>Style me!</p>
+            <button>Toggle style</button>
+        </div>
+    );
+}
